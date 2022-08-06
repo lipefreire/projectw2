caefd15cfbca6a16f14d1b2d44adacf884e27d52
Felipe Freire <felipef12123@gmail.com>
2022-08-06 08:00:46 -0300

    first commit

---
+++ projetopw2/src/main/java/br/edu/ifsertaope/cs/projetoweb/resources/CategoriaResource.java
@@ -0,0 +1,11 @@
+package br.edu.ifsertaope.cs.projetoweb.resources;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@RestController
+@RequestMapping(value="/categorias")
+public class CategoriaResource {
+
+}

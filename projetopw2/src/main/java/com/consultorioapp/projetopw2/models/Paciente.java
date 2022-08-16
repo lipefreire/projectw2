3c587ff6f66cad677111a45979f1154cf3c6f0a1
Felipe Freire <felipef12123@gmail.com>
2022-08-16 12:46:41 -0300

    insert in database

--- projetopw2/src/main/java/com/consultorioapp/projetopw2/models/Paciente.java
+++ projetopw2/src/main/java/com/consultorioapp/projetopw2/models/Paciente.java
@@ -1,11 +1,33 @@
 package com.consultorioapp.projetopw2.models;
 
-public class Paciente {
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Paciente implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	
 	private String nome;
-	private String cpf;		private String telefone;
+	private String cpf;		
+	private String telefone;
 	private String email;
 	private String dataNascimento;
 	
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
 	public String getNome() {
 		return nome;
 	}
